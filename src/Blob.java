01f828ed0b60b7275c6e5c6711053759fc6998d5
Remy navarre <remynav16@gmail.com>
2022-09-07 19:35:53 -0700

    Blob works

---
+++ src/Blob.java
@@ -0,0 +1,70 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Formatter;
+
+
+public class Blob {
+	
+	private String sha1Code; 
+	
+	public Blob(String path) throws IOException {
+		byte[] encoded = Files.readAllBytes(Paths.get(path));
+        String contents=  new String(encoded, StandardCharsets.UTF_8); 
+        String fileString = new String(encoded);
+        sha1Code=getSha1Name(contents);
+        
+        File sha1File = new File("/Users/remynavarre/eclipse-workspace/VersionControl/objects/"+sha1Code);
+        sha1File.createNewFile();
+        
+        FileWriter myWriter = new FileWriter(sha1File.getName());
+        myWriter.write(fileString);
+        myWriter.close();
+        
+	}
+	
+	public String getSha1FileName() {
+		return sha1Code;
+	}
+	
+	private static String getSha1Name(String password)
+	{
+	    String sha1 = "";
+	    try
+	    {
+	        MessageDigest crypt = MessageDigest.getInstance("SHA-1");
+	        crypt.reset();
+	        crypt.update(password.getBytes("UTF-8"));
+	        sha1 = byteToHex(crypt.digest());
+	    }
+	    catch(NoSuchAlgorithmException e)
+	    {
+	        e.printStackTrace();
+	    }
+	    catch(UnsupportedEncodingException e)
+	    {
+	        e.printStackTrace();
+	    }
+	    return sha1;
+	}
+
+	private static String byteToHex(final byte[] hash)
+	{
+	    Formatter formatter = new Formatter();
+	    for (byte b : hash)
+	    {
+	        formatter.format("%02x", b);
+	    }
+	    String result = formatter.toString();
+	    formatter.close();
+	    return result;
+	}
+	
+	
+}
---
+++ src/BlobTester.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+public class BlobTester {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		
+		Blob test= new Blob("/Users/remynavarre/eclipse-workspace/VersionControl/something");
+	}
+
+}
